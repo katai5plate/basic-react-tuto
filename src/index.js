925b91e732e3ecb65ecc2b01aca29fd5768c624a
katai5plate <katai5plate.sub@gmail.com>
2018-05-05 14:52:24 +0900

    init lesson 4

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <div>Hello-WA!</div>
+);
+
+const render = () => ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+render();
