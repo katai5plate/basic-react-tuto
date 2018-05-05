8898c2be9b5f4d199a73a850c9e10ff00e25c876
katai5plate <katai5plate.sub@gmail.com>
2018-05-05 17:29:04 +0900

    lesson 8: Control text box

--- src/index.js
+++ src/index.js
@@ -9,9 +9,19 @@ const MyButton = () => (
     </div>
 );
 
+// 変更後の値
+let textData = "";
+// onChange、つまり値が更新された時の処理
+const setTextData = (event) => {
+    // 変更後の値 = 変更直後の値
+    textData = event.target.value;
+    // これがないと正常に入力操作ができない。
+    // なぜならデータ変更しても自動では再描画されないから。
+    render();
+}
 const MyBox = () => (
     <div>
-        <input type="text" />
+        <input type="text" value={textData} onChange={setTextData} />
     </div>
 );
 
