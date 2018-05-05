bb0d0a8ddafc7c77874bf4df1fea4a4ea993958b
katai5plate <katai5plate.sub@gmail.com>
2018-05-05 17:14:17 +0900

    lesson 8:Display the button

--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const randExt = ()=>Math.random()*10;
-const items = [
-    [
-        { name: "aaa", color: "black", ext: randExt() },
-        { name: "bbb", color: "brown", ext: randExt() },
-        { name: "ccc", color: "red", ext: randExt() },
-        { name: "ddd", color: "blue", ext: randExt() },
-        { name: "eee", color: "green", ext: randExt() },
-    ],
-    [
-        { name: "aaa", color: "gray", ext: randExt() },
-        { name: "bbb", color: "aqua", ext: randExt() },
-        { name: "ccc", color: "orange", ext: randExt() },
-        { name: "ddd", color: "purple", ext: randExt() },
-        { name: "eee", color: "lightgray", ext: randExt() },
-    ],
-];
-
-const Hello = ({ name, color, ext }) => (
+const MyButton = () => (
     <div>
-        <p style={{ color }}>
-            Hello, {name}{"!".repeat(ext)}
-        </p>
+        <button>Click Me!</button>
     </div>
 );
 
 const App = () => (
     <div>
-        {
-            items.map((item) => {
-                return item.map((i)=>(
-                    <Hello name={i.name} color={i.color} ext={i.ext}/>
-                ))
-            })
-        }
+        <MyButton />
     </div>
 );
 
