d98700538547bf4f34fd303d4f4d2acd920a5652
katai5plate <katai5plate.sub@gmail.com>
2018-05-06 23:55:51 +0900

    resolve #9

--- src/index.js
+++ src/index.js
@@ -28,61 +28,76 @@ let editData = "";
 // inputの入力制御・editDataとの同期
 const setEditData = (event) => {
     editData = event.target.value;
-    if(!(/[0-9]/.test(editData))) return;
+    if (!(/[0-9]/.test(editData))) return;
     render();
 }
 // データ追加
 const addData = () => {
-    if(editData == ""){
+    if (editData === "") {
         // 新規の場合
         items.push({ name: nameData, color: colorData });
-    }else{
+    } else {
         // 編集の場合
         items[editData] = { name: nameData, color: colorData };
     }
-    nameData = "";
-    colorData = "";
-    editData = "";
+    [nameData, colorData, editData] = [""];
     render();
 }
 
 // フォーム
-const MyForm = () => (
-    <div>
-        <p>
-            名前：
-            <input type="text" value={nameData} onChange={setNameData} />
-        </p>
-        <p>
-            色：
-            <input type="text" value={colorData} onChange={setColorData} />
-        </p>
-        <p>
-            番号：
-            <input type="text" value={editData} onChange={setEditData} />
-        </p>
-        <button onClick={addData}>Add Data</button>
-    </div>
-);
+//const MyForm = () => (
+class MyForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    名前：
+                    <input type="text" value={nameData} onChange={setNameData} />
+                </p>
+                <p>
+                    色：
+                    <input type="text" value={colorData} onChange={setColorData} />
+                </p>
+                <p>
+                    番号：
+                    <input type="text" value={editData} onChange={setEditData} />
+                </p>
+                <button onClick={addData}>Add Data</button>
+            </div>
+        )
+    }
+}
 
 // 削除可能版
-const Hello = ({ name, color, onEdit, onDelete }) => (
-    <div>
-        <p>
-            <span style={{ color }}>
-                Hello, {name}!
-            </span>
-            <button onClick={() => onEdit()}>EDIT</button>
-            <button onClick={() => onDelete()}>DEL</button>
-        </p>
-    </div>
-);
+class Hello extends React.Component {
+    render() {
+        const props = this.props;
+        const [name, color, onEdit, onDelete] = [
+            props.name,
+            props.color,
+            () => props.onEdit(),
+            () => props.onDelete(),
+        ]
+        return (
+            <div>
+                <p>
+                    <span style={{ color }}>
+                        Hello, {name}!
+                    </span>
+                    <button onClick={onEdit}>EDIT</button>
+                    <button onClick={onDelete}>DEL</button>
+                </p>
+            </div>
+        );
+    }
+}
 // 編集処理
 const editItem = (index) => {
     // 確認メッセージ
-    nameData = items[index].name;
-    colorData = items[index].color;
-    editData = index;
+    const item = items[index];
+    [nameData, colorData, editData] = [
+        item.name, item.color, index
+    ];
     render();
 }
 // 削除処理
@@ -94,21 +109,26 @@ const deleteItem = (index) => {
     render();
 }
 
-const App = () => (
-    <div>
-        <MyForm />
-        {
-            items.map((item, index) => (
-                <Hello
-                    name={item.name}
-                    color={item.color}
-                    onEdit={() => editItem(index)}
-                    onDelete={() => deleteItem(index)}
-                />
-            ))
-        }
-    </div>
-);
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <MyForm />
+                {
+                    items.map((item, index) => (
+                        <Hello
+                            name={item.name}
+                            color={item.color}
+                            onEdit={() => editItem(index)}
+                            onDelete={() => deleteItem(index)}
+                        />
+                    ))
+                }
+            </div>
+        )
+    }
+};
 
 const render = () => ReactDOM.render(
     <App />,
